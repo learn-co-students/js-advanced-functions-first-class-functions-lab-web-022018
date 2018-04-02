aa52ff10f9fbb75b582f53c8ff92c75323781b0c
Chase <chaseit12@gmail.com>
2018-04-02 16:56:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+
+    let arr = []
+     arr.push(drivers[0], drivers[1])  
+     return arr
+
+}
+
+const returnLastTwoDrivers = function(drivers){
+    let arr = []
+     arr.push(drivers[drivers.length-2], drivers[drivers.length-1])  
+     return arr
+}
+
+// PASS IN THE FUNCTIONS LIKE this
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, whichDrivers){
+  return whichDrivers(drivers)
+}
+ 
